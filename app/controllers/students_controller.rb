bdc6f806a5f7de6795aa34023701dc12aa78b8f6
Steve Anderson <prototype909@msn.com>
2020-07-09 03:11:14 +0000

    Automatically backed up by Learn

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -9,6 +9,16 @@ class StudentsController < ApplicationController
     set_student
   end
 
+  def activate
+      set_student
+      if @student.active == true
+         @student.active = false
+      else
+         @student.active = true
+      end
+      @student.save
+      redirect_to student_path(@student)
+    end
 
   private
 
